2bba29448ed42ca383e3fd02001d3c040386343c
Labeeb <farooqi.labeeb07@gmail.com>
2023-03-22 13:53:08 -0600

    Commit

--- # Area of a Circle Calculator.py
+++ # Area of a Circle Calculator.py
@@ -1,7 +1,7 @@
 # Area of a Circle Calculator
 # Labeeb Farooqi
 #2023-03-17
-
+import turtle as turtle
 import math
 
 diameter = input("Enter the diameter of the circle.\nIf you know the radius, press enter and then enter the radius.\n")
@@ -14,10 +14,6 @@ area = math.pi*radius**2
 
 print("The area of the circle is: ", area)
 
-import turtle
-
 turtle.pendown()
 turtle.circle(radius)
 turtle.done()
-turtle.pen
-turtle.write
