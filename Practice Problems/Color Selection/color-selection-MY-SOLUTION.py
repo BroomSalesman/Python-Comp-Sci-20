2134ede0090907c1df2824c800372869a39885b8
Labeeb <farooqi.labeeb07@gmail.com>
2023-03-28 13:39:08 -0600

    new

---
+++ Practice Problems/Color Selection/color-selection-MY-SOLUTION.py
@@ -0,0 +1,17 @@
+# Color Selection
+
+import turtle
+
+# create window, and set it's color
+canvas = turtle.Screen()
+canvas.bgcolor("lightgreen")
+
+#create the turtle, and it's attributes
+bree = turtle.Turtle()
+bree.color("blue")
+bree.pensize(3)
+
+#draw!
+bree.forward(100)
+bree.right(60)
+bree.forward(100)
