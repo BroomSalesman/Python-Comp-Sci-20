b1c1bb2ea64cb8c9f57f9779d7f7be1093ceb4aa
Labeeb <farooqi.labeeb07@gmail.com>
2023-03-28 13:11:19 -0600

    File location changes

--- Quadratic Formula thingy.py
+++
@@ -1,4 +0,0 @@
-y = 21
-x = 3
-quadratic_formula = x^2 + 21*x + y
-print (quadratic_formula)
--- if-try-exception example.py
+++
@@ -1,6 +0,0 @@
-try:
-    x = int(input("Enter a positive integer: "))
-    if x <= 0:
-        raise ValueError("Number should be positive")
-except ValueError as e:
-    print(e)
