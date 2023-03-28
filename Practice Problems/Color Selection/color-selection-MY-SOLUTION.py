ee80d26d6c4de89b98581b7e75fa08587876bbc6
Labeeb <farooqi.labeeb07@gmail.com>
2023-03-28 13:43:29 -0600

    new changes

--- Practice Problems/Color Selection/color-selection-MY-SOLUTION.py
+++ Practice Problems/Color Selection/color-selection-MY-SOLUTION.py
@@ -1,3 +1,14 @@
+'''     Modify the program given below so that before it creates the window, it prompts the 
+    user to enter the desired background color. It should store the user's response in a variable, 
+    and modify the color of the window according to the user's wishes. Do similar changes to allow 
+    the user to set the turtle bree's color as well.
+
+    Hint: you can find a list of permitted color names at https://www.w3schools.com/colors/colors_names.asp . 
+    It includes some quite unusual ones, like “PeachPuff” and “HotPink”.'''
+
+
+
+
 # Color Selection
 
 import turtle
