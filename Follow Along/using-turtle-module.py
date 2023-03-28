4d99c20b21c6c184e424945a320ea96befdcb21c
Labeeb <farooqi.labeeb07@gmail.com>
2023-03-28 13:24:14 -0600

    new

--- Follow Along/using-turtle-module.py
+++ Follow Along/using-turtle-module.py
@@ -6,10 +6,9 @@ canvas = tur.Screen()
 monkey = tur.Turtle()
 monkey.shape("turtle")
 monkey.color("purple")
+monkey.pensize(100)
 
 
 for i in range(4):
-    monkey.forward(175)
+    monkey.forward(10000)
     monkey.left(90)
-
-
