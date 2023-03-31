1d5cb526bce4a35e047d9c1301344a9a1c5fa309
Labeeb <farooqi.labeeb07@gmail.com>
2023-03-31 14:08:55 -0600

    new changes

--- Follow Along/Microbit Stuff/Accelerometer-Stuff.py
+++ Follow Along/Microbit Stuff/Accelerometer-Stuff.py
@@ -1,6 +1,19 @@
 import turtle
 import microbit as mcb
 
+window = turtle.Screen()
+monkey = turtle.Turtle()
+monkey.color("Yellow")
+monkey.shape("Turtle")
+
 while True:
     x = mcb.accelerometer.get_x()
     print(x)
+
+    if x > 300:
+        print("Right!")
+        laura.forward(5)
+    
+    if x < -300:
+        print("LEFT!")
+        laura.backward(5)
