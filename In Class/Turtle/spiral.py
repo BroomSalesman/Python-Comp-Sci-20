882b07d387d6a3bdfe58744ec3be094a9ed234cb
Labeeb <farooqi.labeeb07@gmail.com>
2023-04-04 12:57:13 -0600

    new changes

--- In Class/Turtle/spiral.py
+++ In Class/Turtle/spiral.py
@@ -4,13 +4,13 @@ window = tur.Screen()
 window.bgcolor("lightyellow")
 
 turt = tur.Turtle()
-turt.shape("Turtle")
+turt.shape("turtle")
 turt.color("hotpink")
 turt.pensize(4)
 turt.penup()
 turt.speed(0)
 
-for side in range(2, 100):
+for side in range(2, 500):
     turt.stamp()
     turt.forward(side)
     turt.left(50)
