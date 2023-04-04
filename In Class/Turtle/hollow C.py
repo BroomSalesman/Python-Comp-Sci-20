95561ffcf4645fa3370d007b59cc606d8dfaeffa
Labeeb <farooqi.labeeb07@gmail.com>
2023-04-04 13:06:14 -0600

    new changes

--- In Class/Turtle/hollow C.py
+++ In Class/Turtle/hollow C.py
@@ -20,42 +20,45 @@ turt.pendown()
 turt.forward(200)
 turt.penup()
 
-# Back start point (bottom left outer line), draws bottom outer line
+# Back start point (bottom of left outer line), draws bottom outer line
 turt.goto(0, -100)
 turt.right(90)
 turt.pendown()
 turt.forward(200)
 turt.penup()
 
-# Goes back to
+# Goes to top of left outer line, draws top outer line
 turt.goto(0, 100)
 turt.pendown()
 turt.forward(200)
 turt.penup()
 
-# Done outer perimeter at this point (big C)
+# Done normal C shape (should look like square with no right side)
 
-turt.goto(100, -100)
-turt.right(90)
+# Goes to bottom right point (of bottom outer line), then draws farthest right line (one of the small lines)
+turt.goto(200, -100)
+turt.left(90)
 turt.pendown()
 turt.forward(40)
 turt.penup()
 
-turt.goto(100, 100)
+# Does the same as above, but at the top
+turt.goto(200, 100)
 turt.right(180)
 turt.pendown()
 turt.forward(40)
 turt.penup()
 
-
-turt.goto(100, -60)
+# Goes to bottom short line, and draws bottom inside line
+turt.goto(200, -60)
 turt.right(90)
 turt.pendown()
 turt.forward(160)
 turt.penup()
 
-turt.goto(100, 160)
+# Same as above, but for upper inside line
+turt.goto(200, 60)
 turt.pendown()
-turt.forward
-
+turt.forward(160)
+turt.penup()
 
