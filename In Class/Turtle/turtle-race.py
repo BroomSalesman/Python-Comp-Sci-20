210919506c1029d7059c22105de5a6f8f3db1a70
Labeeb <farooqi.labeeb07@gmail.com>
2023-04-05 13:45:08 -0600

    ben

---
+++ In Class/Turtle/turtle-race.py
@@ -0,0 +1,15 @@
+import turtle
+
+
+#setup window and turtles
+
+window = turtle.Screen()
+window.bgcolor("DarkGreen")
+
+bob = turtle.Turtle()
+bob.shape("turtle")
+bob.color("yellow")
+
+stuart = turtle.Turtle()
+stuart.shape("turtle")
+stuart.color("LightGoldenrod2")
--- Random/turtlecircle.py
+++
@@ -1,8 +0,0 @@
-import turtle
-
-turtle = turtle.Turtle()
-
-turtle.speed(0)
-for i in range(360):
-    turtle.forward(2)
-    turtle.right(1)
