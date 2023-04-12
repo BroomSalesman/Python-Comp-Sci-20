c230f8e21a3f805d1d42fdc69c14cfcb75ec5c46
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2023-04-11 22:29:45 -0600

    New files and new changes

---
+++ Assignments/Mad Lib Assignment/mad-lib.py
@@ -0,0 +1,8 @@
+#################################################################
+# Labeeb Farooqi
+# Computer Science 20
+# April 11, 2023
+#
+# Mad Lib Program
+# Purpose: To create a mad lib through user input and random selection
+################################################################
--- Assignments/Temperature-Conversion/temperature-converter-program.py
+++ Assignments/Temperature-Conversion/temperature-converter-program.py
@@ -26,8 +26,9 @@ possible_temp_units = ["C", "F", "K"]
 # Function to show user what to input their temperature unit as
 def show_options():
     print("\nEnter C for Celsius.")
-    print("\nEnter F for Fahrenheit")
-    print("\nEnter K for Kelvin")
+    print("Enter F for Fahrenheit")
+    print("Enter K for Kelvin")
+
 
 
 
@@ -43,34 +44,35 @@ for dot in range(4):
 # Prints introduction to program and how to use it
 time.sleep(0.5)
 print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThis program, written by Labeeb Farooqi, is designed to convert one unit of temperature to another."
-         "\nTo use it, you must enter what unit you want to convert from (), what unit you would like to" 
-         " convert to, and then enter the temperature to be calculated (which must be a number).")
-time.sleep(2)
+         "\nTo use it, you must enter what unit you want to convert from, what unit you would like to" 
+         " convert to, and then enter the temperature to be calculated.")
+time.sleep(4)
 
 # Next line of code is executed only after users presses enter
-input("\nPress enter if you have finished reading.")
+input("\nPress enter when you have finished reading.")
+
+
 
 
 
 ######  Part 1: Asks user to input unit of temperature to convert FROM ######
 
 time.sleep(1)
-print("\nWhat temperature unit would you like to convert from? Example: CELSIUS to fahrenheit")
+print("\nWhat temperature unit would you like to convert from? EXAMPLE: CELSIUS to fahrenheit")
 time.sleep(1.5)
 show_options()
-time.sleep(2)
+time.sleep(1)
 temp_unit_from = input("\nEnter your selection:  ").upper()
 
 
-
-
 # Checks if user entered valid option
-if not temp_unit_from in possible_temp_units:
-    print("\n\nThat is not an option")
-    time.sleep(1.5)
-    show_options()
-    time.sleep(1.5)
-    temp_unit_from = input("\nRenter your selection:  ").upper()
+if not temp_unit_from in possible_temp_units:    
+    while not temp_unit_from in possible_temp_units:
+        print("\n\nThat is not an option")
+        time.sleep(1.5)
+        show_options()
+        time.sleep(1.5)
+        temp_unit_from = input("\nTry again:  ").upper()
 
 
 # Creates word version of temperature unit for outputting string purposes
@@ -82,7 +84,7 @@ if temp_unit_from == "C":
 elif temp_unit_from == "F":
     time.sleep(1.25)
     temp_unit_from_word = "Fahrenheit"
-    print(f"\n\nYou chose  {temp_unit_from_word}.")
+    print(f"\n\nYou chose {temp_unit_from_word}.")
 
 else:
     time.sleep(1.25)
@@ -91,26 +93,40 @@ else:
     
 
 
+
+
+
 ######  Part 2: Asks user to input unit of temperature to convert TO ######
-time.sleep(0.65)
-print("\n\nWhat unit of temperature would you like to convert to? Example: Celsius to FAHRENHEIT")
+time.sleep(2)
+print("\n\nWhat unit of temperature would you like to convert to? EXAMPLE: Celsius to FAHRENHEIT")
+time.sleep(1.2)
 show_options()
+time.sleep(1)
 temp_unit_to = input("\nEnter your selection:  ").upper()
+
+
 
 
 # If input is the same unit of temperature selected
 if not temp_unit_to in possible_temp_units:
-    if temp_unit_to == temp_unit_from:
-        print("\n\nYou have already picked that as the first temperature unit.")
-        time.sleep(0.8)
-        print("\nChoose a different temperature unit.")
-        time.sleep(1)
-        temp_unit_from = input("\nRenter your selection:  ").upper()
-    else:
-        print("That is not a valid")
+    while not temp_unit_to in possible_temp_units:
+        if temp_unit_to == temp_unit_from:
+            print("\n\nYou have already picked that as the first temperature unit.")
+            time.sleep(0.8)
+            print("\nChoose a different temperature unit.")
+            time.sleep(1)
+            temp_unit_to = input("\nTry again:  ").upper()
+        else:
+            print("That is not a valid option")
+            time.sleep(1.5)
+            show_options()
+            time.sleep(1.5)
+            temp_unit_to = input("Try again:  ")
+
+
+
     
-    
-    # Creates word version of the chosen temperature unit
+# Creates word version of the chosen temperature unit
 if temp_unit_to == "C":
     time.sleep(1.25)
     temp_unit_to_word = "Celsius"
@@ -128,9 +144,12 @@ else:
 
 
 
-########## Part 3:  Asks user to enter temperature ##########
-temp_to_be_converted = float(input("\n\nEnter temperature (should be a number):"))
 
+
+
+########## Part 3:  Asks user to enter temperature ##########
+temp_to_be_converted = (input("\n\nEnter temperature (must be a number):"))
+temp_to_be_converted = float(temp_to_be_converted)
 # Checks if user entered valid input
 while not type(temp_to_be_converted) == float or type(temp_to_be_converted) == int:
     time.sleep(0.5)
