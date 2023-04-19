f055d110b56991ed32b493141bbb2efbdca0cf87
Labeeb <farooqi.labeeb07@gmail.com>
2023-04-19 14:13:25 -0600

    new changes

--- easygui choicebox snippet.py
+++ easygui choicebox snippet.py
@@ -1,12 +1,12 @@
 import easygui 
 
 # Define a list of numbers
-mainscale = []
+banana = []
 for i in range(81):
-    mainscale.append(i+20)
+    banana.append(i+20)
 
 # Use choicebox to display the numbers and allow the user to choose one
-selected_number = easygui.choicebox("Please select a number", "Number selection", mainscale)
+selected_number = easygui.choicebox("Please select a number", "Number selection", banana)
 
 # Print the selected number to the console
 print(f"You selected {selected_number}")
