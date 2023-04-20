76af39ad8a68e2b3af2543e6dcadcc492b588206
Labeeb <farooqi.labeeb07@gmail.com>
2023-04-20 13:31:10 -0600

    folder changes?

---
+++ Textbook Practice Problems/3. Python - Functions in Detail/1. Fruitful Functions and Variable Scope/1.1 Warmup Problem.py
@@ -0,0 +1,26 @@
+import turtle
+
+def draw_rectangle(some_turtle, length, width):
+    """Draws a rectangle using turtle
+
+    Args:
+        some_turtle (turtle.Turtle() object): A turtle for drawing
+        length (int, float): Determines length of the rectangle
+        width (int, float): Detemines width of the rectangle
+    """
+    
+    for side in [width, length, width, length]:
+        some_turtle.forward(side)
+        some_turtle.left(90)
+
+canvas = turtle.Screen()
+canvas.bgcolor("dark gray")
+
+bob = turtle.Turtle()
+bob.pensize(4)
+bob.color("sky blue")
+bob.shape("turtle")
+
+for rectangle in range (4):
+    draw_rectangle(bob, 50, 70)
+    bob.right(90)
