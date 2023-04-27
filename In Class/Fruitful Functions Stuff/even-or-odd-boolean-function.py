7eeeae5c96bdb388553c9ebe2e23912fa0deb2ff
Labeeb <farooqi.labeeb07@gmail.com>
2023-04-27 11:57:13 -0600

    changes

--- In Class/Fruitful Functions Stuff/even-or-odd-boolean-function.py
+++ In Class/Fruitful Functions Stuff/even-or-odd-boolean-function.py
@@ -1,20 +1,2 @@
-# pylint: disable=missing-docstring, invalid-name
-def ver1_is_divisible(x, y):
-    if x % y == 0:
-        result = True
-    else:
-        result = False
-
-    return result
-
-print (ver1_is_divisible(10, 5))
-print(ver1_is_divisible(7, 3))
-
-def ver2_is_divisible(x, y):
-    return x % y == 0
-
-if ver2_is_divisible(10, 5):
-    print("That works")
-
-else:
-    print("That doesn't work.")
+def even_or_odd(num):
+    
