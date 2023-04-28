6eb0213574405c3bed3985afba44291dfe456b7c
Labeeb <farooqi.labeeb07@gmail.com>
2023-04-28 15:44:46 -0600

    new changes

--- Python Stuff/In Class/Fruitful Functions Stuff/even-or-odd-boolean-function.py
+++ Python Stuff/In Class/Fruitful Functions Stuff/even-or-odd-boolean-function.py
@@ -1,2 +1,11 @@
 def even_or_odd(num):
-    
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+this_shalt_be_the_number = 0
+while even_or_odd(this_shalt_be_the_number):
+    this_shalt_be_the_number = input("What shalt be the number?  ")
+    this_shalt_be_the_number = int(this_shalt_be_the_number)
--- Python Stuff/Textbook Practice Problems/3. Python - Functions in Detail/4. Boolean Functions/4.2. Randomly Walking Turtles.py
+++ Python Stuff/Textbook Practice Problems/3. Python - Functions in Detail/4. Boolean Functions/4.2. Randomly Walking Turtles.py
@@ -10,8 +10,10 @@ def is_on_screen(window, a_turtle):
     x = a_turtle.xcor()
     y = a_turtle.ycor()
     
-    if x > left_edge and 
-
+    if x > left_edge and x <  right_edge and y < top_edge and y >  bottom_edge:
+        return True
+    else:
+        return False
 
 canvas = turtle.Screen()
 canvas.bgcolor("Dark Grey")
@@ -26,4 +28,12 @@ bob.pensize(5)
 while is_on_screen(canvas, bob):
     choice = random.randrange(0, 3)
     if choice == 0: #forward
-        bob.forward()
+        bob.forward(50)
+    elif choice == 1:  #left
+        bob.left(90)
+        bob.forward(50)
+    else: #right
+        bob.right(90)
+        bob.forward(50)
+        
+print("All done!")
