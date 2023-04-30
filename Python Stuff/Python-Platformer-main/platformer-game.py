30df6c6ae1150459af8a499e6f5bebb9c320d9dd
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2023-04-30 11:52:27 -0600

    commit

--- Python Stuff/Python-Platformer-main/platformer-game.py
+++ Python Stuff/Python-Platformer-main/platformer-game.py
@@ -1,5 +1,5 @@
 # pylint: disable= missing-function-docstring, unused-import, unused-variable, wrong-import-order, unused-argument, redefined-outer-name
-# pylint: disable = consider-using-sys-exit
+# pylint: disable = consider-using-sys-exit, invalid-name
 import os
 import random
 import math
@@ -11,13 +11,45 @@ pygame.init()
 
 pygame.display.set_caption("Platformer Game")
 
-
-BG_COLOR = (255, 255, 255)  # here until I implement my own background
 WIDTH, HEIGHT = 1000, 800
 FPS = 120   # Supposed to be 60, if game doesnt work make sure to check this out
 PLAYER_VEL = 5  # Speed of player
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
 
 def get_background(name):  #name is color of background
     image = pygame.image.load(join("assets", "Background", name))
@@ -26,21 +58,21 @@ def get_background(name):  #name is color of background
 
     for i in range(WIDTH // width + 1):
         for j in range(HEIGHT // height + 1):
-            pos = [i * width, j * height]
+            pos = (i * width, j * height)
             tiles.append(pos)
 
     return tiles, image
 
-def draw(window, background):
+def draw(window, background, bg_image):
     for tile in background:
-        window.blit()
+        window.blit(bg_image, tile)
 
     pygame.display.update()
 
+
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image = get_background(Yellow.png)
-
+    background, bg_image = get_background("Yellow.png")
 
     run = True
     while run:
@@ -50,6 +82,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
 
     pygame.quit()
     quit()
