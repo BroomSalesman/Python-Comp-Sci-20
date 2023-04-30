08284df52c6770a4579a2627b021682fc4e1d7d2
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2023-04-30 10:50:27 -0600

    json settings changes, platformer-game.py changes

--- Python Stuff/Python-Platformer-main/platformer-game.py
+++ Python Stuff/Python-Platformer-main/platformer-game.py
@@ -1,12 +1,16 @@
+# pylint: disable= missing-function-docstring, unused-import, unused-variable, wrong-import-order, unused-argument, redefined-outer-name
+# pylint: disable = consider-using-sys-exit
 import os
 import random
 import math
 import pygame
 from os import listdir
 from os.path import isfile, join
+
 pygame.init()
 
 pygame.display.set_caption("Platformer Game")
+
 
 BG_COLOR = (255, 255, 255)  # here until I implement my own background
 WIDTH, HEIGHT = 1000, 800
@@ -16,20 +20,25 @@ PLAYER_VEL = 5  # Speed of player
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def get_background(name):  #name is color of background
-    image = pygame.image.load(join("assets", "Background", name)) 
+    image = pygame.image.load(join("assets", "Background", name))
+    
+
+
 
 def main(window):
     clock = pygame.time.Clock()
 
     run = True
     while run:
-        # Might make game super fast, fps may not determine smoothness, remember for later
-        clock.tick(FPS)
+        clock.tick(FPS) #If game super fast, lower FPS line: 17
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
