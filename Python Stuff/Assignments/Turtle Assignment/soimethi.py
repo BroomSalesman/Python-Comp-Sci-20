c7bb91d99d7730e44350c75d86202d5068b7b410
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2023-05-14 21:39:45 -0600

    new changes

--- Python Stuff/Assignments/Turtle Assignment/soimethi.py
+++ Python Stuff/Assignments/Turtle Assignment/soimethi.py
@@ -1,22 +1,31 @@
-import microbit
+
 import time
 import turtle
 import easygui
 
 
 
-temperature = easygui.choicebox(
-    msg="Enter a temperature",
-    title="Thermometer Temperature",
-    choices=iter(range(-5, 36)),
-)
+temperature = easygui.choicebox(msg="Enter a temperature", title="Thermometer Temperature", choices=iter(range(-5, 36)))
+
+
 
 canvas = turtle.Screen()
-canvas.bgcolor("dark grey")
+canvas.bgcolor("black")
 
 bob = turtle.Turtle()
 bob.shape("turtle")
 bob.color("yellow")
 bob.pensize(5)
+
+
+bob.goto(0, -300)
+bob.left(90)
+turtle.trace(G0)
+for i in range(360):
+    bob.forward
+
+bob.forward(10)
+bob.penup()
+bob.forward()
 
 
