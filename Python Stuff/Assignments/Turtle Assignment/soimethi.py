b82225671178868fb97a5a7f0e24ddab0d7db749
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2023-05-15 22:40:15 -0600

    dsasd

--- Python Stuff/Assignments/Turtle Assignment/soimethi.py
+++ Python Stuff/Assignments/Turtle Assignment/soimethi.py
@@ -8,7 +8,7 @@ __temperature__ = easygui.choicebox(msg="Enter a temperature", title="Thermomete
 
 
 canvas = turtle.Screen()
-canvas.bgcolor("black")
+canvas.bgcolor("gray")
 
 bob = turtle.Turtle()
 bob.color("yellow")
@@ -17,9 +17,14 @@ bob.pensize(3)
 bob.penup()
 bob.goto(0, -300)
 bob.left(90)
+bob.pendown()
 
-bob.circle(10, 10
+bob.speed(0.9)
+bob.circle(radius = 50, steps = 360)
+
 
 bob.forward(10)
 bob.penup()
 
+
+time.sleep(4)
