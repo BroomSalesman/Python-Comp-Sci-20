7e19a18abb94442d2d90ec0e9f7642593f74cfb6
Labeeb Farooqi <farooqi.labeeb07@gmail.com>
2023-06-01 14:08:07 -0600

    adsdas

--- Python Stuff/Assignments/Pig Latin Assignment/pig-latin.py
+++ Python Stuff/Assignments/Pig Latin Assignment/pig-latin.py
@@ -4,18 +4,43 @@
 #Then loop back and join all of them
 # When I rejoin the translated string, I will first join the translated sub strings with no spaces
 
+# If the word starts with a vowel, end = yay
+# Else: end = ay
+
 def piece_string(string):
     return string.split(" ")
 
-def separate_punctuation(list_of_strings):
-    for word in list_of_strings:
-        if word[-1] in ".,!?":
-            list_of_strings[list_of_strings.index(word)] = word[:-1] + "♠" + word[-1:]
-    
-    for item in list_of_strings:
-        if type(item) == 'list':
-            return True
 
+
+
+
+def separate_punctuation(string_list):
+    for word in string_list:
+        if word[-1] in ".,!?":
+            string_list[string_list.index(word)] = word[:-1] + "♠" + word[-1:]
+        
+    for word in string_list:
+        if "♠" in word:
+            string_list[string_list.index(word)] = word.split("♠")
+
+def translate_string(string):
+    if string[0] in "AEIOUYaeiouy:
+        suffix = "yay
+    else:
+        suffix = "ay"
+    
+    if string[0] in 
+
+
+def translate_sublist_strings(string_list):
+    for item in string_list:
+        if type(item) = "list":
+             for string in item:
+                 if string in ".,!?":
+                     continue
+                 elif:
+                     if 
+                 
 
 the_string = "The green! monkey ran over the tree or something like that"
 
