bd02a80dbce24c0ec9db1830aeb1a599a6a0f887
Your Name <farooqi.labeeb07@gmail.com>
2024-01-05 12:33:46 -0600

    xsxc

--- CCC Practice Problems/Special-Event.py
+++ CCC Practice Problems/Special-Event.py
@@ -4,12 +4,17 @@ available = []
 some_number = 0
 
 for i in range(people):
-    schedules.append(input().split())
+    schedules.append(input())
 
 print(schedules)
 
 for i in range(5):
     for j in range(people):
-        if schedules[j][i] == 'Y':
+        if schedules[j][i]== 'Y':
             some_number += 1
+    available.append(some_number)
+    some_number = 0
+
+max =
+print(inde)
 
