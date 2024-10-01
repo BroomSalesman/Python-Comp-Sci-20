fcc0b7391ce2add987e41748ed69eef7accf1437
Your Name <farooqi.labeeb07@gmail.com>
2024-10-01 16:54:44 -0600

    changes

--- 30-Level/Interactive Scene Project/sketch.js
+++ 30-Level/Interactive Scene Project/sketch.js
@@ -1,5 +1,3 @@
-import {createPallete} from 'functions/colorPallete.js';
-
 let pencolor;
 let pensize = 5;
 
@@ -26,6 +24,68 @@ function draw() {
 
 
 //Placeholder for colors in the pallete
+
+
+
+
+
+export function pltBlock(x, y, size, color) {
+  fill(color);
+  square(x, y, size);
+}
+
+function createPallete() {
+
+  //pallete area
+  line (40, 0 , 40, 1000);
+  fill('lightgray');
+  rect(0, 0, 40, 1000);
+
+  //black
+  pltBlock(5, 5, pltsize, 'black');
+
+  //gray
+  pltBlock(5, 40, pltsize, 'gray');
+
+  //white
+  pltBlock(5, 75, pltsize, 'white');
+
+  //red
+  pltBlock(5, 110, pltsize, 'red');
+
+  //orange
+  pltBlock(5, 145, pltsize, 'orange');
+
+  //yellow
+  pltBlock(5, 180, pltsize, 'yellow');
+
+  //lime
+  pltBlock(5, 215, pltsize, 'lime');
+
+  //green
+  pltBlock(5, 250, pltsize, 'green');
+
+  //turquoise
+  pltBlock(5, 285, pltsize, 'turquoise');
+
+  //sky blue
+  pltBlock(5, 320, pltsize, 'skyblue');
+
+  //blue
+  pltBlock(5, 355, pltsize, 'royalblue');
+
+  //darkblue
+  pltBlock(5, 390, pltsize, 'darkblue');
+
+  //violet
+  pltBlock(5, 425, pltsize, 'indigo');
+
+  //lavendar
+  pltBlock(5, 460, pltsize, '#a392cb');
+
+  //pink
+  pltBlock(5, 495, pltsize, 'hotpink');
+}
 
 function coloringPen() {
   circle(mouseX, mouseY,  pensize);
