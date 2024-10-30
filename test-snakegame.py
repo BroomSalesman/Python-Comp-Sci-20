0f127d3312fedb11ab01a130bb73280344018e0b
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2024-10-30 12:23:32 -0600

    sdads

--- test-snakegame.py
+++ test-snakegame.py
@@ -1,7 +1,3 @@
-
-
-
-
 import random
 import turtle
 
@@ -9,37 +5,30 @@ WIDTH = 800
 HEIGHT = 800
 FOOD_SIZE = 20
 
-
 def go_up():
     global snake_direction
     if snake_direction != "down":
         snake_direction = "up"
-
 
 def go_down():
     global snake_direction
     if snake_direction != "up":
         snake_direction = "down"
 
-
 def go_right():
     global snake_direction
     if snake_direction != "left":
         snake_direction = "right"
-
 
 def go_left():
     global snake_direction
     if snake_direction != "right":
         snake_direction = "left"
 
-
 def random_food_position():
     x = random.randint(-WIDTH // 2 + FOOD_SIZE, WIDTH // 2 - FOOD_SIZE)
     y = random.randint(-HEIGHT // 2 + FOOD_SIZE, HEIGHT // 2 - FOOD_SIZE)
     return [x, y]
-
-
 
 def touching_food():
     x = new_head[0]
@@ -50,7 +39,7 @@ def touching_food():
 def move_snake():
     global food_pos, snake, new_head
 
-    #Next position for the HEAD of the snake
+    #next position for head of snake
     new_head = snake[-1].copy()
 
     if snake_direction == "up":
@@ -58,19 +47,21 @@ def move_snake():
     elif snake_direction == "down":
         new_head[1] -= 20
     elif snake_direction == "left":
-        new_head[0] -=20
-    elif snake_direction == "right":
+        new_head[0] -= 20
+    else:
         new_head[0] += 20
 
-    #Check collision and reset
+    #check self-collision
     if new_head in snake:
         start()
+
     else:
         snake.append(new_head)
 
         if touching_food():
             food_pos = random_food_position()
             food.goto(food_pos)
+
         else:
             snake.pop(0)
 
@@ -86,31 +77,32 @@ def move_snake():
 
 
 def start():
-    global snake_direction, snake, food_pos
+    global snake_direction, food_pos, snake
     snake_direction = "up" #default direction
     snake = [[0, 0], [0, 20], [0, 40], [0, 60], [0, 80]]
-    food_pos = random_food_position()
-    food.goto(food_pos)
+    food_pos =random_food_position()
+    food.goto(food_pos) #the pen stamping the food
     move_snake()
 
 
-#start showing the second step to setting up
+
+# Window
 window = turtle.Screen()
 window.setup(WIDTH, HEIGHT)
 window.title("Fun Snake Game")
 window.bgcolor("green")
 window.tracer(0)
 
-# Pen draws our snake
+# Pen draws the snake
 pen = turtle.Turtle("square")
 pen.penup()
 
-#This pen draws our food
+# Food
 food = turtle.Turtle("circle")
-food.color('red')
+food.color("red")
 food.penup()
 
-#Event handler (calls control functions)
+# Event handlers
 window.listen()
 window.onkey(go_up, "w")
 window.onkey(go_down, "s")
@@ -118,4 +110,4 @@ window.onkey(go_left, "a")
 window.onkey(go_right, "d")
 
 start()
-turtle.done()
+window.exitonclick()
