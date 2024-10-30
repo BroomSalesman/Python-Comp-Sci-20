8b65d0c487f5422aa54f2b220276a65d8c01173c
BroomSalesman <105088158+BroomSalesman@users.noreply.github.com>
2024-10-30 09:54:10 -0600

    sdsad

---
+++ snakegame.py
@@ -0,0 +1,19 @@
+import random
+import turtle
+import math
+
+WIDTH = 800
+HEIGHT = 800
+
+
+
+
+#start showing the second step to setting up
+window = turtle.Screen()
+window.setup(WIDTH, HEIGHT)
+window.title("Fun Snake Game")
+window.bgcolor("green")
+window.tracer(0)
+
+
+
